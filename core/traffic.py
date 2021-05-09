9d307f83edcad2e15b779b7a49dfe30e7e403c27
Luca G. Pinta <lucapinta@live.it>
2021-05-09 23:09:31 +0200

    sniffing from pcap with SYN filter

--- core/traffic.py
+++ core/traffic.py
@@ -22,7 +22,26 @@ class TrafficCatcher(threading.Thread):
     A thread used for capturing traffic and saving data of interest into DB
     """
 
- 
+    def __init__(self, stype, source):
+        super(TrafficCatcher, self).__init__()
+        self.stype = stype
+        self.source = source
+
+    def run(self):
+        if self.stype == 0: #Live capture
+            print("live capture")
+        
+        else: #PCAP FIle sniffing
+            # Filter for  TCP/UDP layer and check SYN = 1 in packet flags
+            pkts = sniff(offline=self.source, lfilter = lambda x: x.haslayer(TCP) and x[TCP].flags & SYN)   
+            #pkts = rdpcap(self.source)
+
+            count = 0
+            for packet in pkts:
+                count+=1
+                print(f"{packet[0][1].src} ==> {packet[0][1].dst}")
+            
+            print("Total SYN packets: ", count)
 
 class TrafficAnalyzer(threading.Thread):
     """
