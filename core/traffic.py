395cd906b3dee141c2b72af19ad885466bea1002
Edoardo Ermini <edoardo.ermini@protonmail.com>
2021-05-10 19:24:04 +0200

    added comments and improved readability

--- core/traffic.py
+++ core/traffic.py
@@ -1,68 +1,44 @@
 import threading
-from scapy import *
-from scapy.utils import rdpcap
 from scapy.sendrecv import sniff
-from scapy.layers.inet import TCP, UDP
-
-# TCP-Flags
-FIN = 0x01
-SYN = 0x02
-RST = 0x04
-PSH = 0x08
-ACK = 0x10
-URG = 0x20
-ECE = 0x40
-CWR = 0x80
-
-
-        #pkt.show() # debug statement
+from scapy.layers.inet import TCP
 
 
 class TrafficCatcher(threading.Thread):
-
-
     """
     A thread used for capturing traffic and saving data of interest into DB
     """
 
-    def __init__(self, stype, source):
+    def __init__(self, source, live_capture=False):
         super(TrafficCatcher, self).__init__()
-        self.stype = stype
+
+        self.live_capture = live_capture
         self.source = source
 
+        self.syn_counter = 0
 
     def callback(self, pkt):
-        if pkt.haslayer(TCP) and pkt[TCP].flags & SYN:
-            try:
-                print(f"{pkt[0][1].src} ==> {pkt[0][1].dst} at time {pkt.time}")
-            
-            except:
-                pass
-            
+        """
+        Called by sniff every time it reads a packet.
+        If given packet is a TCP packet and has SYN flag set to 1
+        increases syn packets counter
+
+        :param pkt: packet read
+        """
+
+        syn = 0x02
+
+        if pkt.haslayer(TCP) and pkt[TCP].flags & syn:
+            self.syn_counter += 1
+
+            print(f"{pkt[0][1].src} ==> {pkt[0][1].dst} at time {pkt.time}")
 
     def run(self):
-        if self.stype == 0: #Live capture
-            print(self.source)
-            print( "live  capture")
-            pkts = sniff(iface=self.source,  prn = self.callback, lfilter = lambda x: x.haslayer(TCP) and x[TCP].flags & SYN)   
-            counter = 0
-            for packet in pkts:
-                counter+=1
-               
-            print("Total SYN packets live: ", counter)
-        
-        else: #PCAP FIle sniffing
-            # Filter for  TCP/UDP layer and check SYN = 1 in packet flags
-            #pkts = sniff(offline=self.source, lfilter = lambda x: x.haslayer(TCP) and x[TCP].flags & SYN)  
-            pkts = sniff(offline=self.source, prn = self.callback, lfilter = lambda x: x.haslayer(TCP) and x[TCP].flags & SYN)  
-            #pkts = rdpcap(self.source)
 
-            count = 0
-            for packet in pkts:
-                count+=1
-                #print(f"{packet[0][1].src} ==> {packet[0][1].dst} at time {packet[0][1].time}")
-            
-            print("Total SYN packets: ", count)
+        if self.live_capture:
+            sniff(iface=self.source,  prn=self.callback)
+        else:
+            sniff(offline=self.source, prn=self.callback)
+
 
 class TrafficAnalyzer(threading.Thread):
     """
