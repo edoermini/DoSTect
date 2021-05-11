5957d1487d906a41b17251024b22d2b36ad283e3
Edoardo Ermini <edoardo.ermini@protonmail.com>
2021-05-11 23:33:10 +0200

    added reset of ewma and g values after DDoS detection

--- core/traffic.py
+++ core/traffic.py
@@ -9,40 +9,42 @@ class TrafficAnalyzer:
     A thread used for capturing traffic and saving data of interest into DB
     """
 
-    def __init__(self, source, time_interval=5, alpha=0.5, beta=0.99, live_capture=False):
-        self.live_capture = live_capture
+    def __init__(self, source, live_capture=False, threshold=10, sigma=100, time_interval=5, alpha=0.5, beta=0.99):
         self.source = source
+        self.live_capture = live_capture
+
+        self.threshold = threshold
+        self.sigma = sigma
+        self.time_interval = time_interval
+        self.alpha = alpha
+        self.beta = beta
 
         self.syn_counter_lock = threading.Lock()
         self.syn_counter = 0
 
-        self.alpha = alpha
-        self.beta = beta
-        self.threshold = 5
-        self.sigma = 100
+        self.__last_g = 0
+        self.__last_ewma = 0
 
-        self.time_interval = time_interval
-
-        self.last_g = 0
-        self.last_ewma = 0
+        self.__threshold_exceeded = False
 
     def __ewma(self, syn_count) -> float:
-        new_ewma = self.beta*self.last_ewma + (1-self.beta)*syn_count
-        self.last_ewma = new_ewma
+        new_ewma = self.beta * self.__last_ewma + (1 - self.beta) * syn_count
+        self.__last_ewma = new_ewma
 
         return new_ewma
 
     def __g(self, syn_count):
-        new_g = self.last_g + ((self.alpha*self.last_ewma)/(self.sigma**2))*(syn_count - self.last_ewma - self.alpha*self.last_ewma/2)
+        new_g = self.__last_g + ((self.alpha * self.__last_ewma) / (self.sigma ** 2)) * (
+                    syn_count - self.__last_ewma - self.alpha * self.__last_ewma / 2)
 
         self.__ewma(syn_count)
 
         if new_g > 0:
-            self.last_g = new_g
+            self.__last_g = new_g
         else:
-            self.last_g = 0
+            self.__last_g = 0
 
-        return self.last_g
+        return self.__last_g
 
     def __counter_reader(self):
         syn_count = 0
@@ -54,9 +56,20 @@ class TrafficAnalyzer:
         val = self.__g(syn_count)
         print(val)
 
-        
         if val > self.threshold:
-            self.last_g = 0
+            print("[!] Warning DDoS detected")
+            self.__last_g = 0
+            self.__threshold_exceeded = True
+        else:
+            # not DDoS detected
+
+            if self.__threshold_exceeded:
+                # DDoS stopped
+                # last check recorded an attack
+
+                self.__last_ewma = 0
+                self.__threshold_exceeded = False
+
         threading.Timer(self.time_interval, self.__counter_reader).start()
 
     def __callback(self, pkt):
@@ -71,22 +84,18 @@ class TrafficAnalyzer:
         syn = 0x02
 
         if pkt.haslayer(TCP) and pkt[TCP].flags & syn:
-
             with self.syn_counter_lock:
                 self.syn_counter += 1
 
     def start(self):
         """
-
-        :param time_interval:
-        :return:
+        Starts packet capturing and analyzing
         """
 
         # start thread that runs every time_interval seconds
         self.__counter_reader()
 
         if self.live_capture:
-            sniff(iface=self.source,  prn=self.__callback)
+            sniff(iface=self.source, prn=self.__callback)
         else:
             sniff(offline=self.source, prn=self.__callback)
-
